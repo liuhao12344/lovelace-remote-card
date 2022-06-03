d2e0e3266e95dc720f929a90e93add6087131138
liuhao12344 <39158320+liuhao12344@users.noreply.github.com>
2022-06-04 03:02:33 +0800

    Update remote-card.js

--- remote-card.js
+++ remote-card.js
@@ -283,9 +283,7 @@ class RemoteCard extends HTMLElement {
             </div>
         </div>
         <div id="bottom_buttons"></div>
-        <div id="bottom_input">
-            <input type="text" />
-        </div>
+     
     `
         return html;
     }
